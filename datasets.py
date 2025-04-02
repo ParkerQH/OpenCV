7297f2c934d4e2c26854ee537ba5eef77c97a5ba
ParkerQH <blueskyaoa7@gmail.com>
2025-04-02 10:12:05 +0900

    데이터 수정 & 헬멧 데이터 학습

--- datasets.py
+++ datasets.py
@@ -8,6 +8,6 @@ load_dotenv()
 # Roboflow API 키로 데이터셋 다운로드
 api_key = os.getenv('MY_API_KEY') 
 rf = Roboflow(api_key=api_key)
-project = rf.workspace("kickbord").project("-lfqkf")
+project = rf.workspace("kickbord").project("helmet-dxttm")
 version = project.version(1)
 dataset = version.download("yolov8")
