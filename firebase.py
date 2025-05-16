997625ca717c68d16800dfe9a67d5d516fbe83b3
ParkerQH <blueskyaoa7@gmail.com>
2025-05-16 13:00:14 +0900

    YOLO firebase 연동 시작

---
+++ firebase.py
@@ -0,0 +1,10 @@
+import firebase_admin
+from firebase_admin import credentials, db
+
+cred = credentials.Certificate('serviceAccountKey.json')
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://kts-dataset-default-rtdb.firebaseio.com'
+})
+
+ref = db.reference('/')
+print(ref.get())
